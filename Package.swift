44d140cca76eb683441d277232ae5b43074a3ad3
Fabio Martino <martinofabio@libero.it>
2026-01-30 20:27:56 +0100

    fix(spm): update binaries to ios-8.0.2-spm-fix2

--- Package.swift
+++ Package.swift
@@ -18,13 +18,13 @@ let package = Package(
     targets: [
         .binaryTarget(
         name: "Capacitor",
-        url: "https://github.com/cap-kit/capacitor/releases/download/spm-xcframework-fix/Capacitor.xcframework.zip",
-        checksum: "b9eab62a1bf2c582adde3e3ddb5452515f7d77ba09525f03ec5f256f99cc9971"
+        url: "https://github.com/cap-kit/capacitor/releases/download/ios-8.0.2-spm-fix2/Capacitor.xcframework.zip",
+        checksum: "5ece93a4bbf85013e347fa8207771210d01a9655ad3994b4506cf9049200d398"
         ),
         .binaryTarget(
         name: "Cordova",
-        url: "https://github.com/cap-kit/capacitor/releases/download/spm-xcframework-fix/Cordova.xcframework.zip",
-        checksum: "74cd6f9351bec2833db0e24cb60997584a5b7174ce85f5eea467e465f6444c62"
+        url: "https://github.com/cap-kit/capacitor/releases/download/ios-8.0.2-spm-fix2/Cordova.xcframework.zip",
+        checksum: "ffc6874f3c90e7a74587c137345a6aeccd8215db8b2aa2a0b513e4435d8043c1"
         ),
     ]
 )
